ec7ca609352b27db5eed5072a1fde94df1511969
nikithaduvvuri <97437377+nikithaduvvuri@users.noreply.github.com>
2022-05-19 11:29:02 +0530

    num=int(input()) rem=s=0; len=len(str(num)) n=num; while(num>0):     rem=num%10;     s+=int(rem**len);     num=num//10;     len-=1; if(s==n):     print("True") else:     print("False")

---
+++ Disarium_number.py
@@ -0,0 +1,13 @@
+num=int(input())
+rem=s=0;
+len=len(str(num))
+n=num;
+while(num>0):
+    rem=num%10;
+    s+=int(rem**len);
+    num=num//10;
+    len-=1;
+if(s==n):
+    print("True")
+else:
+    print("False")
