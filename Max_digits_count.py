e6dd6d3499a69727de73a06ba02182fb2e9c4649
nikithaduvvuri <97437377+nikithaduvvuri@users.noreply.github.com>
2022-07-01 12:00:58 +0530

    n=int(input()) arr=list(map(int,input().split())) c=0 max=0 for i in range(n):     if len(str(arr[i]))>max:         max=len(str(arr[i])) for i in range(n):     if len(str(arr[i]))==max:         c=c+1 print(c)

---
+++ Max_digits_count.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=list(map(int,input().split()))
+c=0
+max=0
+for i in range(n):
+    if len(str(arr[i]))>max:
+        max=len(str(arr[i]))
+for i in range(n):
+    if len(str(arr[i]))==max:
+        c=c+1
+print(c)
